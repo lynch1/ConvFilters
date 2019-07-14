c1136d8884c9f0a4f00c24ea9e509356b1ccce06
lynch1 <danlynch001@gmail.com>
2019-07-14 14:13:01 -0400

    Changed layout of ui. Got opening existing filters as plain text files working. Started working on saving existing filters as plain text files.

--- convfilter.cpp
+++ convfilter.cpp
@@ -12,3 +12,40 @@ ConvFilter::~ConvFilter()
 {
     delete ui;
 }
+
+
+
+void ConvFilter::on_saveSubimageButton_released()
+{
+    QString subimageFilename = QFileDialog::getOpenFileName(this, "Save a subimage");
+    QFile subimageFile(subimageFilename);
+
+    // Error handling for opening a sub image file.
+    if (!subimageFile.open(QIODevice::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Couldn't save filter" + subimageFile.errorString());
+        return;
+    }
+    // Read text in the plain text input fields.
+//    QTextStream outFile(&subimageFile);
+//    QString newFilterValues = "test";
+//    QString filterValues = inFile.readAll();
+//    ui->subimageValuesTextBox->setPlainText(filterValues);
+    subimageFile.close();
+}
+
+void ConvFilter::on_loadSubimageButton_released()
+{
+    QString subimageFilename = QFileDialog::getOpenFileName(this, "Open a subimage");
+    QFile subimageFile(subimageFilename);
+    currentFilter = subimageFilename;
+    // Error handling for opening a sub image file.
+    if (!subimageFile.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Couldn't load filter" + subimageFile.errorString());
+        return;
+    }
+    // Read in file contents
+    QTextStream inFile(&subimageFile);
+    QString filterValues = inFile.readAll();
+    ui->subimageValuesTextBox->setPlainText(filterValues);
+    subimageFile.close();
+}
--- convfilter.h
+++ convfilter.h
@@ -2,21 +2,39 @@
 #define CONVFILTER_H
 
 #include <QMainWindow>
+// For opening files
+#include <QFile>
+#include <QString>
+#include <QTextStream>
+#include <QFileDialog>
+// For giving user error messages
+#include <QMessageBox>
 
 namespace Ui {
 class ConvFilter;
 }
 
+// ConvFilter's base class is a Q_OBJECT
 class ConvFilter : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    // Default constructore with no parent
     explicit ConvFilter(QWidget *parent = nullptr);
+    // Destructor
     ~ConvFilter();
 
+private slots:
+    void on_saveSubimageButton_released();
+
+    void on_loadSubimageButton_released();
+
 private:
+    // The UI Form
     Ui::ConvFilter *ui;
+    QString currentFilter = "";
+    double *filterValues[9];
 };
 
-#endif // CONVFILTER_H
+#endif//CONVFILTER_H
