0abe33d9d414ebadedebbd70040392f08bb5605d
lynch1 <danlynch001@gmail.com>
2019-07-14 20:47:47 -0400

    Got saving subimages/filters working. Realizing I should add some example filter quick keys later once I get most of it working.

--- convfilter.cpp
+++ convfilter.cpp
@@ -17,19 +17,22 @@ ConvFilter::~ConvFilter()
 
 void ConvFilter::on_saveSubimageButton_released()
 {
-    QString subimageFilename = QFileDialog::getOpenFileName(this, "Save a subimage");
+    QString subimageFilename = QFileDialog::getSaveFileName(this, "Save this subimage");
+    // Open a QFile object to handle file I/O
     QFile subimageFile(subimageFilename);
-
-    // Error handling for opening a sub image file.
-    if (!subimageFile.open(QIODevice::WriteOnly | QFile::Text)){
-        QMessageBox::warning(this, "Warning", "Couldn't save filter" + subimageFile.errorString());
+    // Error handling for opening a write only file.
+    if (!subimageFile.open(QIODevice::WriteOnly)){
+        QMessageBox::warning(this, "Warning", "Couldn't save subimage as text file" + subimageFile.errorString());
         return;
     }
     // Read text in the plain text input fields.
-//    QTextStream outFile(&subimageFile);
-//    QString newFilterValues = "test";
-//    QString filterValues = inFile.readAll();
-//    ui->subimageValuesTextBox->setPlainText(filterValues);
+    // outFile is a new instance of a QTextStream object. It's kind of like the iostream objects in the stdio library
+    QTextStream outFile(&subimageFile);
+    // Placeholder QString variable for holding what was written in the text field of the ui
+    QString newFilterValueString = ui->subimageValuesTextBox->toPlainText();
+    // Dump the string into the new file. QTextStream objects also have the overloaded << operator
+    outFile << newFilterValueString;
+    // Close the file
     subimageFile.close();
 }
 
